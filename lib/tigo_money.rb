1a70d73b97e6c736088c38f164be50de1cffb6be
Exequiel Rozas <exequiel98@gmail.com>
2018-04-03 23:18:53 -0400

    Gemspec

--- lib/tigo_money.rb
+++ lib/tigo_money.rb
@@ -1,4 +1,35 @@
-require "tigo_money/version"
+require 'base64'
+require 'json'
+require 'mcrypt'
+require 'openssl'
+require 'savon'
+
+require 'tigo_money/charge'
+require 'tigo_money/client'
+require 'tigo_money/configuration'
+require 'tigo_money/encryptor'
+require 'tigo_money/decryptor'
+require 'tigo_money/errors'
+require 'tigo_money/formatter'
+require 'tigo_money/response'
+require 'tigo_money/status'
+require 'tigo_money/version'
+
+require 'tigo_money/async_payment_response'
+require 'tigo_money/status_check_response'
+require 'tigo_money/sync_payment_response'
+
+
+module TigoMoney
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+end
 
 module TigoMoney
   # Your code goes here...
--- tigo_money.gemspec
+++ tigo_money.gemspec
@@ -33,5 +33,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_dependence ""
+  spec.add_dependency "base64"
+  spec.add_dependency "json"
+  spec.add_dependency "mcrypt"
+  spec.add_dependency "openssl"
+  spec.add_dependency "savon"
 end
