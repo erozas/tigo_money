7f31faaf239680616235dc1c27cabdde5fada056
Exequiel Rozas <exequiel98@gmail.com>
2018-04-04 18:26:08 -0400

    Correct async payment code

--- lib/tigo_money/async_payment_response.rb
+++ lib/tigo_money/async_payment_response.rb
@@ -1,11 +1,35 @@
 module TigoMoney
   class AsyncPaymentResponse < Response
-    attr_accessor :formatted_response
+    attr_accessor :response
 
-    def self.formatted_response response
-      resp = Response.from_savon response
-      resp_body = resp.raw_body[:solicitar_pago_asincrono_response][:return]
-      final_resp = JSON.generate(Formatter.format_response(Decryptor.decrypt(resp_body)))
+    def initialize(response)
+      @response = response
+    end
+
+    def formatted
+      resp = self.class.from_savon(@response)
+      resp_body = resp.raw_body[:solicitar_pago_response][:return]
+      handle_response Formatter.format_response(Decryptor.decrypt(resp_body))
+    end
+
+    private
+
+    def handle_response(resp)
+      response_code = resp["response_code"].to_i
+      if response_code == 0
+        handle_error_response(resp)
+      else
+        handle_successful_response(resp)
+      end
+    end
+
+    def handle_successful_response(response)
+      JSON.generate(response)
+    end
+
+    def handle_error_response(response)
+      puts "Handling error response"
+      puts response
     end
   end
 end
